c41e8d5f3e1303641c8f93a08f4933e45691efc4
sean creighton <sean.creighton@gmail.com>
2017-05-31 12:35:13 +0100

    Ok I have reworked the code base quite a bit
    The C++ projects had hard-coded links to specific installed versions of Postgres. I have made all the C++ projects use relative paths. I also went and tried to go and find the required references, namely the lib and include files for the following versions:
    
    9.x x86
    9.x x64
    8.x x86
    8.x x64 (this one I could not find)
    
    I included this data in the code base (bar 8.x x64). I rebuilt the C++ projects which regenerated the missing dlls and copied them over to the C# folder.

---
+++ Npgsql.BulkCopy/PostgresqlCode/Version8/x86/include/server/catalog/index.h
@@ -0,0 +1,85 @@
+/*-------------------------------------------------------------------------
+ *
+ * index.h
+ *	  prototypes for catalog/index.c.
+ *
+ *
+ * Portions Copyright (c) 1996-2008, PostgreSQL Global Development Group
+ * Portions Copyright (c) 1994, Regents of the University of California
+ *
+ * $PostgreSQL: pgsql/src/include/catalog/index.h,v 1.75.2.1 2008/11/13 17:42:19 tgl Exp $
+ *
+ *-------------------------------------------------------------------------
+ */
+#ifndef INDEX_H
+#define INDEX_H
+
+#include "nodes/execnodes.h"
+
+
+#define DEFAULT_INDEX_TYPE	"btree"
+
+/* Typedef for callback function for IndexBuildHeapScan */
+typedef void (*IndexBuildCallback) (Relation index,
+												HeapTuple htup,
+												Datum *values,
+												bool *isnull,
+												bool tupleIsAlive,
+												void *state);
+
+/* Action code for index_set_state_flags */
+typedef enum
+{
+	INDEX_CREATE_SET_READY,
+	INDEX_CREATE_SET_VALID
+} IndexStateFlagsAction;
+
+
+extern Oid index_create(Oid heapRelationId,
+			 const char *indexRelationName,
+			 Oid indexRelationId,
+			 IndexInfo *indexInfo,
+			 Oid accessMethodObjectId,
+			 Oid tableSpaceId,
+			 Oid *classObjectId,
+			 int16 *coloptions,
+			 Datum reloptions,
+			 bool isprimary,
+			 bool isconstraint,
+			 bool allow_system_table_mods,
+			 bool skip_build,
+			 bool concurrent);
+
+extern void index_drop(Oid indexId);
+
+extern IndexInfo *BuildIndexInfo(Relation index);
+
+extern void FormIndexDatum(IndexInfo *indexInfo,
+			   TupleTableSlot *slot,
+			   EState *estate,
+			   Datum *values,
+			   bool *isnull);
+
+extern void setNewRelfilenode(Relation relation, TransactionId freezeXid);
+
+extern void index_build(Relation heapRelation,
+			Relation indexRelation,
+			IndexInfo *indexInfo,
+			bool isprimary,
+			bool isreindex);
+
+extern double IndexBuildHeapScan(Relation heapRelation,
+				   Relation indexRelation,
+				   IndexInfo *indexInfo,
+				   bool allow_sync,
+				   IndexBuildCallback callback,
+				   void *callback_state);
+
+extern void validate_index(Oid heapId, Oid indexId, Snapshot snapshot);
+
+extern void index_set_state_flags(Oid indexId, IndexStateFlagsAction action);
+
+extern void reindex_index(Oid indexId);
+extern bool reindex_relation(Oid relid, bool toast_too);
+
+#endif   /* INDEX_H */
